0b3a8c3c9044647d6d7cb31c018a52aec4aeedae
Abhinav Shrestha <139767616+abhinavshr@users.noreply.github.com>
2026-05-02 10:57:02 +0545

    feat: integrate badges fetching and display in profile page

--- src/api/Level.js
+++ src/api/Level.js
@@ -1,4 +1,4 @@
 import api from "./axios";
 
-export const getMyLevel = () =>
-  api.get("/level/me");
+export const getMyLevel  = () => api.get("/level/me");
+export const getMyBadges = () => api.get("/level/badges");
